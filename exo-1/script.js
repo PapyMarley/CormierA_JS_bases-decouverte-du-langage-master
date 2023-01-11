d7b3a9c2b55045fe428e12e8124c6e3dc04137cb
PapyMarley <axel.cormier@laposte.net>
2023-01-11 16:55:15 +0100

    avancé exo 2 (non fonctionnel)

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,12 @@
 // you can write js here
 console.log('hello from file');
+
+let kelvins = prompt("Bonjour, quel est la température aujourd'hui ?")
+
+const kelvins = 294; // initialise la variable "kelvins" à la valeur 294
+const celsius = kelvins - 273; // initialise la variable "celsius" qui converti la valeur "kelvin" en valeur "celsius"
+let fahrenheits = celsius * (9/5) + 32 // initialise la variable "fahrenheits" et converti la valeur "celsius" en valeur "fahrenheits"
+
+fahrenheits = Math.floor(fahrenheits);
+
+console.log(fahrenheits);
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,3 @@
 // you can write js here
 console.log('exo-2');
+const myDate = new Date();
