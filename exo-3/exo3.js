ab58321379f5086a459802a3f78ee9ace7da317f
PapyMarley <axel.cormier@laposte.net>
2023-01-12 14:41:22 +0100

    exo 3 et 4 en cours

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,36 @@
 // you can write js here
 console.log('exo-3');
+
+const playerInput = prompt("Your choice ( rock, paper, scissors) ?");
+let playerChoice = playerInput;
+
+/* Player's block */
+function getPlayerChoice(playerInput) {
+    //vérifie que le joueur choississent l'une des 3 propositions
+    console.log(playerInput.toLowerCase());
+}
+
+/* Computer's block */
+function getComputerChoice(){
+    //génére un choix aléatoire pour le PC
+    computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
+    return computerChoice;
+}
+
+
+/* Result's block */
+function findWinner(playerChoice, ComputerChoice){
+    if (playerChoice === scissors){
+    } else if (playerChoice == ComputerChoice) { // Si le joueur et le PC font le même choix
+        console.log("It's a tie!"); 
+    } else if (playerChoice == rock) { // Si le joueur choisi "rock"
+        if (ComputerChoice == paper) { // et que l'ordinateur choisi "paper"
+            console.log("You win!");
+        }
+    }
+}
+
+getPlayerChoice(playerInput);
+getComputerChoice();
+findWinner(playerChoice, computerChoice);
