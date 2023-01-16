3c859f401aa82ccec6d74025de63621177e0711f
PapyMarley <axel.cormier@laposte.net>
2023-01-16 15:38:09 +0100

    exo_5_correctif-exo_6_sans_bonus

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -2,23 +2,24 @@
 
 console.log('exo-5');
 
-const input = prompt('Please enter your phrase: ');
-const upperInput = input.toUpperCase();
+const input = prompt('Please enter your phrase: '); // step 1
+const vowels = ["A", "E", "I", "O", "U", "Y"]; // step 2
+let resultArray = new Array; // step 3
+const upperInput = input.toUpperCase(); 
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const vowels = ["A", "E", "I", "O", "U", "Y"];
-
-let resultArray = new Array;
+upperInput.indexOf(""); // step 8
 
 
-for (let i = upperInput.length - 1; i >= 0; i -= 1) {
+
+for (let i = upperInput.length - 1; i >= 0; i -= 1) { // step 4 et 6
     for (let j = vowels.length - 1; j >= 0; j -= 1){
         if (upperInput[i] === vowels[j]){
             console.log(`${i}. ${upperInput[i]}`);
-            resultArray.push(`${upperInput[i]}`);
+            resultArray.push(`${upperInput[i]}`); // step 7
         }        
     }    
 }
 
-upperInput.indexOf("");
 
-console.log(resultArray);
+
+console.log(resultArray); 
--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -11,7 +11,106 @@ var joeInfo = {
 // ! Ne faites pas de modification au dessus de cette ligne !
 
 // 1. Afficher le nombre de voitures de Joe
-
+console.log(joeInfo.cars.length);
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
-
+joeInfo.bathrooms = 1;
+console.log(joeInfo.bathrooms);
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
+joeInfo.garage = true;
+console.log(joeInfo.garage);
+
+const team = {
+    players: [
+        {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 30,
+        },
+        {
+            firstName: 'Jane',
+            lastName: 'Doe',
+            age: 28,
+        },
+        {
+            firstName: 'Mary',
+            lastName: 'Doe',
+            age: 24,
+        },
+        {
+            firstName: 'Eugene',
+            lastName: 'Doe',
+            age: 74,
+        }
+    ],
+    games: [
+        {
+            opponent: 'Team 1',
+            teamPoints: 42,
+            opponentPoints: 12,
+        },
+        {
+            opponent: 'Team 2',
+            teamPoints: 12,
+            opponentPoints: 6,
+        },
+        {
+            opponent: 'Team 3',
+            teamPoints: 70,
+            opponentPoints: 28,
+        },
+        {
+            opponent: 'Team 4',
+            teamPoints: 64,
+            opponentPoints: 26,
+        },
+    ],
+}
+
+function addPlayers (fName, lName, Age){
+    firstName = fName;
+    lastName = lName;
+    age = Age;
+    return {
+        firstName,
+        lastName,
+        age,
+    }
+}
+
+addPlayers ('Rick', 'Asley', '43');
+addPlayers ('Jack', 'Rayan', '37');
+
+function addGames(addOpponent, addTeamPoints, addOpponentPoints) {
+    opponent = addOpponent;
+    teamPoints = addTeamPoints;
+    opponentPoints = addOpponentPoints;
+
+
+}
+
+addGames('Team 4', 80, 38);
+addGames('Team 5', 100, 50);
+
+let res1 = 0;
+
+for (let key in team.games){
+    res1 += team.games[key].teamPoints;
+};
+console.log(res1);
+
+let res2 = 0;
+
+for (let key in team.games){
+    res2 += team.games[key].opponentPoints;
+};
+console.log(res2);
+
+let res3 = 0;
+
+for (let key in team.players){
+    if (team.players[key].age > res3){
+        res3 = team.players[key].age;
+    }
+};
+
+console.log(res3);
